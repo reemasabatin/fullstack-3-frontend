958ded336cec4355dace4f5074ecfc514ddfb582
Reema Sabatin <67538563+reemasabatin@users.noreply.github.com>
2023-05-16 09:20:05 +0800

    PageDescription

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
-import { Container } from "@mui/system";
+import { Container, TableContainer } from "@mui/material";
 import Header from "./Header";
 import Footer from "./Footer";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Header></Header>
+      <Header title="John Doe">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+      </Header>
       <Container fixed>
         <main>{children}</main>
       </Container>
--- src/pages/projects page/projects.js
+++
@@ -1,3 +0,0 @@
-export default function projects() {
-  return <h1>projects</h1>;
-}
