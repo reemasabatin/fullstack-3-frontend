ece91c4fd2dbfc207ec33a31d36e5edf8d116a4f
Reema Sabatin <67538563+reemasabatin@users.noreply.github.com>
2023-05-17 13:19:51 +0800

    Add Projects using SSR

--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -4,15 +4,13 @@ export default function handler(req, res) {
       _id: 1,
       name: "Camp Site",
       description: "Nucamp Campsite is a simple camp site website",
-      imageUrl:
-        "https://d33wubrfki0l68.cloudfront.net/19c708670a1f21231c1e2efa6ba38dbf52b15343/3237e/assets/jpeg/dopefolio.jpeg",
+      imageUrl: "https://",
     },
     {
       _id: 2,
       name: "Elida",
       description: "Elida is a landing page for an apartment building",
-      imageUrl:
-        "https://d33wubrfki0l68.cloudfront.net/9199afe42f789dbddb324ed3edd326e080e693c1/28f54/assets/jpeg/wilsonport.jpeg",
+      imageUrl: "https://",
     },
   ]);
 }
